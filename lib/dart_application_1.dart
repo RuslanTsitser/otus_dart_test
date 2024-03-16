79b4fa70fcd7da1a141f7fa57b3c387aa37653ca
RuslanTsitser <cicer_ruslan@mail.ru>
2024-03-16 19:54:08 +0300

    Add async and stream functions for calculations and add test cases

--- lib/dart_application_1.dart
+++ lib/dart_application_1.dart
@@ -1,3 +1,15 @@
 int calculate() {
   return 6 * 7;
 }
+
+Future<int> calculateAsync() async {
+  await Future.delayed(Duration(seconds: 1));
+  return 6 * 7;
+}
+
+Stream<int> calculateStream() async* {
+  for (var i = 0; i < 10; i++) {
+    await Future.delayed(Duration(milliseconds: 100));
+    yield i;
+  }
+}
--- test/dart_application_1_test.dart
+++ test/dart_application_1_test.dart
@@ -5,4 +5,83 @@ void main() {
   test('calculate', () {
     expect(calculate(), 42);
   });
+
+  group(
+    'calculateAsync',
+    () {
+      test('calculateAsync with await', () async {
+        expect(await calculateAsync(), 42);
+      });
+
+      test('calculateAsync with completion', () {
+        expect(calculateAsync(), completion(42));
+      });
+
+      test('calculateAsync with expectLater', () async {
+        await expectLater(calculateAsync(), completion(42));
+      });
+    },
+  );
+
+  group(
+    'calculateStream',
+    () {
+      test('calculateStream with await', () async {
+        expect(
+          await calculateStream().toList(),
+          <int>[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        );
+      });
+
+      test('calculateStream with expect', () {
+        expect(
+          calculateStream(),
+          emitsInOrder(<int>[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+        );
+      });
+
+      test('calculateStream with expectLater', () async {
+        await expectLater(
+          calculateStream(),
+          emitsInOrder(<int>[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+        );
+      });
+
+      test('calculateStream with expectLater and await', () async {
+        final stream = calculateStream().asBroadcastStream();
+        await expectLater(stream, emits(0), skip: false);
+        await expectLater(stream, emits(0), skip: true, reason: 'Already emitted');
+        await expectLater(stream, emits(1));
+        await expectLater(stream, emits(2));
+      });
+    },
+  );
+
+  group(
+    'expectAsync',
+    () {
+      test('expectAsync', () {
+        final callback = expectAsync0(() => print('callback'));
+        callback();
+      });
+
+      test('expectAsync with count', () {
+        final callback = expectAsync0(() => print('callback'), count: 2);
+        callback();
+        callback();
+      });
+
+      test('expectAsync with max', () {
+        final callback = expectAsync0(() => print('callback'), max: 3);
+        callback();
+        callback();
+      });
+
+      test('expectAsync1', () {
+        final callback = expectAsync1((int value) => print('callback $value'), count: 2);
+        callback(42);
+        callback(24);
+      });
+    },
+  );
 }
